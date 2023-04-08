8f2d78791671d61f8559677253cdf197d9c40286
HANSDEMBERGERSAMONGILAILAI <127167275+HANSDEMBERGERSAMONGILAILAI@users.noreply.github.com>
2023-04-08 13:26:56 +0700

    modul11

---
+++ src/Hansdemberger080423/Kuda.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hansdemberger080423;
+
+/**
+ *
+ * @author HP
+ */
+public class Kuda extends LivingThing {
+    public void berjalan(){
+        System.out.println("berjalan Kuda");  
+    }  
+}
