b4bda5063c0f258850a4e18590c499f50d750347
HANSDEMBERGERSAMONGILAILAI <127167275+HANSDEMBERGERSAMONGILAILAI@users.noreply.github.com>
2023-04-15 14:34:13 +0700

    baru

---
+++ Hansdemberger150423/Model/AnggotaDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Hansdemberger150423.Model;
+import java.util.List;
+/**
+ *
+ * @author HP
+ */
+public interface AnggotaDao {
+    void save(Anggota anggota);
+    void update(int index, Anggota anggota);
+    void delete(int index);
+    Anggota getAnggota(int index);
+    List<Anggota>getAll();
+    
+}
