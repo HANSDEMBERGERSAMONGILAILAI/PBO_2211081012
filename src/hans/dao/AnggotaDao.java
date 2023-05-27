d93af6f520bc775fd8debd3c7077178b71260ff6
HANSDEMBERGERSAMONGILAILAI <127167275+HANSDEMBERGERSAMONGILAILAI@users.noreply.github.com>
2023-05-27 11:27:07 +0700

    commit

---
+++ src/hans/dao/AnggotaDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hans.dao;
+
+import hans.model.Anggota;
+
+/**
+ *
+ * @author HP
+ */
+public interface AnggotaDao {
+    void insert (Anggota anggota) throws Exception;
+}
---
+++ src/hans/db/DbHelper.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hans.db;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import hans.dao.*;
+import hans.model.*;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author HP
+ */
+public class DbHelper {
+    private static Connection connection;
+    
+    public  static Connection getConnection()throws SQLException{
+        if(connection==null){
+            MysqlDataSource dataSource = new MysqlDataSource();
+            dataSource.setURL("jdbc:mysql://localhost/pbo_2211081012");
+            dataSource.setUser("root");
+            dataSource.setPassword("");
+            connection = dataSource.getConnection();
+            
+        }
+        return connection;
+        
+    }
+    public static void main(String[] args){
+        try{
+            connection = DbHelper.getConnection();
+            AnggotaDao dao = new AnggotaDaoImpl(connection);
+            Anggota anggota = new Anggota("A001", "hans", "mentawai","L");
+            dao.insert(anggota);
+            JOptionPane.showMessageDialog(null, "Koneksi Ok");
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+}
