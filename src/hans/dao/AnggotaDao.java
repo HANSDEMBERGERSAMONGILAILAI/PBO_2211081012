bb9c3cd1ba24b4c7c37173c36d6d91230b5408f3
HANSDEMBERGERSAMONGILAILAI <127167275+HANSDEMBERGERSAMONGILAILAI@users.noreply.github.com>
2023-06-15 10:13:27 +0700

    baru

--- src/hans/dao/AnggotaDao.java
+++ src/hans/dao/AnggotaDao.java
@@ -5,11 +5,15 @@
 package hans.dao;
 
 import hans.model.Anggota;
-
+import java.util.List;
 /**
  *
  * @author HP
  */
 public interface AnggotaDao {
     void insert (Anggota anggota) throws Exception;
+    void update(String index, Anggota anggota) throws Exception;
+    void delete(String index) throws Exception;
+    Anggota getAnggota(String kodeanggota) throws Exception;
+    List<Anggota>getAll() throws Exception;
 }
